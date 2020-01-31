ad1cad4438bc2d3e7a479950e8a9908045e9e6ea
忘却的旋律 <fhenn@163.com>
2020-01-31 10:32:21 +0800

    regex

--- src/string/regex/Finding.java
+++ src/string/regex/Finding.java
@@ -7,12 +7,12 @@ public class Finding {
     public static void main(String[] args) {
         Matcher m = Pattern.compile("\\w+").matcher("Evening is full of the linnet's wings");
         while (m.find()){
-            System.out.println(m.group() + " ");
+            System.out.print(m.group() + " ");
         }
         System.out.println();
         int i = 0;
         while (m.find(i)){
-            System.out.println(m.group() + " ");
+            System.out.print(m.group() + " ");
             i++;
         }
     }
---
+++ src/string/regex/Group.java
@@ -0,0 +1,13 @@
+package string.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Group {
+    static public final String POEM = "nicew icebie iewcbiweu  iecbwi";
+
+    public static void main(String[] args) {
+        Matcher m = Pattern.compile("").matcher(POEM);
+        
+    }
+}
