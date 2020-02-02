0cbd613051d002e897a09e17b0b1fdfc70ddc3b4
忘却的旋律 <fhenn@163.com>
2020-02-02 09:15:01 +0800

    Generators泛型方法

---
+++ src/generics/demo8/Generators.java
@@ -0,0 +1,16 @@
+package generics.demo8;
+
+import generics.demo5.Generator;
+
+import java.util.Collection;
+
+public class Generators {
+    public static <T> Collection<T> fill(Collection<T> coll, Generator<T> gen,int n){
+        for (int i = 0; i < n; i++) {
+            coll.add(gen.next());
+        }
+        return coll;
+    }
+    
+
+}
